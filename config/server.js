3ce020c73c6141ac7d47138bb71caf69b1cb55a9
sanojsilva <silvasanoj@gmail.com>
2021-10-03 00:54:48 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b213f2dc738b96b792d14163cbc4fa57'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e961b6a2-a502-44c8-85c3-8b7b6489ec65'
+};
